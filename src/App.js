0b5f307a7ed8676c64c0e50663be2fc0c40ae7f9
cxrvxxxx <brentempasisx3@gmail.com>
2023-10-08 23:45:55 +0800

    homepage layout & banner

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import styles from './styles/App.module.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './common/Navbar';
+import Home from './pages/Home';
 
 const App = () => {
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  );
+    return (
+        <div className={`${styles.App}`}>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
