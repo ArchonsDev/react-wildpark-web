c3580a6e31a8289308e11f9d1c46439d57e94bd3
cxrvxxxx <brentempasisx3@gmail.com>
2023-10-09 21:13:03 +0800

    modified home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,32 +1,25 @@
 import React from "react";
 import styles from './style.module.css';
-import banner from '../../images/homepage.jpg';
 
 const Home = () => {
-    const bannerStyle = {
-        backgroundImage: `url(${banner})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        height: '25vh',
-        width: 'auto',
-        position: 'relative',
-    }
 
-    const textStyle = {
-        textDecoration: 'none',
-        color: '#e9e9e9',
-        position: 'absolute',
-        bottom: '0.3em',
-        right: '1em'
-    }
-
-    return (
-        <div className="container-fluid px-0">
-            <div className="container-fluid" style={bannerStyle}>
-                <small style={textStyle} href="http://www.freepik.com">Designed by upklyak / Freepik</small>
-            </div>
+  return (
+    <>
+      <div className={`container-fluid px-0  ${styles['banner']} ${styles.Home}`}>
+        <div className="row">
+          <div className="col-lg-12">
+            <span className={`d-flex justify-content-center ${styles['tagline']}`}>
+              <p>Finding a <u className={`${styles['highlighted-text']}`}>parking space</u> has never been this easy!</p>
+            </span>
+          </div>
         </div>
-    );
+        <div className={`container d-flex justify-content-center ${styles.content}`}>
+          <span>Find a guaranteed parking space everytime using our Automated Parking Management and reservation system.</span>
+        </div>
+        <small className={`${styles['credits']}`} href="http://www.freepik.com">Header designed by upklyak / Freepik</small>
+      </div >
+    </>
+  );
 }
 
 export default Home;
