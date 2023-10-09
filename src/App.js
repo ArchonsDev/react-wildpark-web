708dbabb4d5b5a38aed502008d1d4660bc0dfb75
Dawn <dydawnraine@gmail.com>
2023-10-09 22:07:37 +0800

    added register page, modified navbar and app.js slightly

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import styles from './styles/App.module.css';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './common/Navbar';
-import Home from './pages/Home';
+import styles from "./styles/App.module.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./common/Navbar";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
 
 const App = () => {
-    return (
-        <div className={`${styles.App}`}>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </div>
-    );
-}
+  return (
+    <div className={`${styles.App}`}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <Home />
+            </>
+          }
+        />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
