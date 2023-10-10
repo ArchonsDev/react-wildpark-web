9e148f83ef38f8f1b78d56be024b34697e229672
Dawn <dydawnraine@gmail.com>
2023-10-10 20:50:43 +0800

    fixed register page background, made input box similar to figma

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -4,78 +4,72 @@ import logo from "../../images/logo.png";
 
 const Register = () => {
   return (
-    <>
-      <div className={`${styles.registerBackground}`}>
-        <div className={`container ${styles.formContainer}`}>
-          <form>
-            {/* Register section: Title */}
-            <div className={`${styles.titleForm}`}>
-              <img src={logo} className={`${styles.logoStyle}`} />
-              <h3 className={`${styles.registerTitle}`}>Register</h3>
-            </div>
-            {/* Register section: Name */}
-            <div className="form-group d-flex">
-              <div className={`mr-2 ${styles.inputNameBox}`}>
-                <label htmlFor="inputFirstname">First name</label>
-                <input
-                  type="text"
-                  className={`form-control ${styles.inputMargin}`}
-                  id="inputFirstname"
-                  aria-describedby="fnameHelp"
-                />
-              </div>
-              <div>
-                <label htmlFor="inputLastname">Last name</label>
-                <input
-                  type="text"
-                  className={`form-control ${styles.inputMargin}`}
-                  id="inputLastname"
-                  aria-describedby="lnameHelp"
-                />
-              </div>
-            </div>
-
-            {/* Register section: Email */}
-            <div className="form-group">
-              <label for="inputEmail">Email address</label>
+    <div className={`container-fluid ${styles.customContainer}`}>
+      <div className={`${styles.formBox}`}>
+        <form>
+          {/* Register section: Title */}
+          <div className={`${styles.titleForm}`}>
+            <img src={logo} className={`${styles.logoStyle}`} />
+            <h3 className={`${styles.registerTitle}`}>Register</h3>
+          </div>
+          {/* Register section: Name */}
+          <div className="form-group d-flex">
+            <div className={`mr-2 ${styles.inputNameBox}`}>
+              <label htmlFor="inputFirstname">First name</label>
               <input
-                type="email"
-                class={`form-control ${styles.inputMargin}`}
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
+                type="text"
+                className={`form-control ${styles.inputMargin}`}
+                id="inputFirstname"
+                aria-describedby="fnameHelp"
               />
             </div>
-
-            {/* Register section: Password */}
-            <div className="form-group">
-              <label for="inputPassword">Password</label>
+            <div>
+              <label htmlFor="inputLastname">Last name</label>
               <input
-                type="password"
-                class={`form-control ${styles.inputMargin}`}
-                id="inputPassword"
+                type="text"
+                className={`form-control ${styles.inputMargin}`}
+                id="inputLastname"
+                aria-describedby="lnameHelp"
               />
             </div>
-
-            <div className="form-group">
-              <label for="inputConfirmPassword">Confirm Password</label>
-              <input
-                type="password"
-                class={`form-control ${styles.inputMargin}`}
-                id="inputConfirmPassword"
-              />
+          </div>
+          {/* Register section: Email */}
+          <div className="form-group">
+            <label for="inputEmail">Email address</label>
+            <input
+              type="email"
+              class={`form-control ${styles.inputMargin}`}
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          {/* Register section: Password */}
+          <div className="form-group">
+            <label for="inputPassword">Password</label>
+            <input
+              type="password"
+              class={`form-control ${styles.inputMargin}`}
+              id="inputPassword"
+            />
+          </div>
+          <div className="form-group">
+            <label for="inputConfirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              class={`form-control ${styles.inputMargin}`}
+              id="inputConfirmPassword"
+            />
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-auto">
+              <button type="submit" className={`${styles.submitButton}`}>
+                Continue
+              </button>
             </div>
-
-            <div className="row justify-content-center">
-              <div className="col-auto">
-                <button type="submit" className={`${styles.submitButton}`}>
-                  Submit
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
