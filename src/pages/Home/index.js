6b9b6b3310330041cb8cf1067df28368d867eaeb
cxrvxxxx <brentempasisx3@gmail.com>
2023-10-10 23:18:17 +0800

    homepage edit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,21 +4,15 @@ import styles from './style.module.css';
 const Home = () => {
 
   return (
-    <>
-      <div className={`container-fluid px-0  ${styles['banner']} ${styles.Home}`}>
-        <div className="row">
-          <div className="col-lg-12">
-            <span className={`d-flex justify-content-center ${styles['tagline']}`}>
-              <p>Finding a <u className={`${styles['highlighted-text']}`}>parking space</u> has never been this easy!</p>
-            </span>
-          </div>
+    <div className={`${styles.Home} container-fluid px-0`}>
+      <div className={`${styles.backdrop}`}>
+        <div className={`${styles.content} container d-flex flex-column justify-content-center align-items-center`}>
+            <p className={`${styles.tagline} col-md-10`}>Finding a <u className={`${styles['highlighted-text']}`}>parking space</u> has never been this easy!</p>
+            <div className={`${styles['cta-button']} px-5 py-3 mt-3`}>Get Started</div>
         </div>
-        <div className={`container d-flex justify-content-center ${styles.content}`}>
-          <span>Find a guaranteed parking space everytime using our Automated Parking Management and reservation system.</span>
-        </div>
-        <small className={`${styles['credits']}`} href="http://www.freepik.com">Header designed by upklyak / Freepik</small>
-      </div >
-    </>
+      </div>
+      <small className={`${styles['credits']}`} href="http://www.freepik.com">Header designed by upklyak / Freepik</small>
+    </div>
   );
 }
 
