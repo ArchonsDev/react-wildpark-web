69a72b4542d951ea0b879aeefe796320d26e6c7e
ced <cedrickhermoso12345@gmail.com>
2023-11-15 11:33:48 +0800

    creating support branch

---
+++ src/pages/Support/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./style.module.css";
+
+const Support = () => {
+  return (
+    <div>
+      <div className={styles.backdrop}>
+        <div className={styles.aboutHeader}>
+          <span>About Us</span>
+        </div>
+      </div>
+    </div>
+  );
+};
