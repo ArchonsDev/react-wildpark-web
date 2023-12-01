4b5fa7ad778ba9717f263337a7212a33ac3f320a
Dawn <dydawnraine@gmail.com>
2023-12-01 20:59:25 +0800

    brower tab stuff-name changes per page and added our logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Cookie from "js-cookie";
 
@@ -15,8 +15,39 @@ import styles from "./styles/App.module.css";
 const App = () => {
   const location = useLocation();
 
-  const [sessionUser, setSessionUser] = useState(Cookie.get('userAccount'));
+  const [sessionUser, setSessionUser] = useState(Cookie.get("userAccount"));
   const sessionUserContextValue = { sessionUser, setSessionUser };
+
+  useEffect(() => {
+    let tabName = "";
+    switch (location.pathname) {
+      case "/":
+        tabName = "Home";
+        break;
+      case "/about":
+        tabName = "About Us";
+        break;
+      case "/register":
+        tabName = "Register";
+        break;
+      case "/support":
+        tabName = "Support";
+        break;
+      case "/dashboard":
+        tabName = "Dashboard";
+        break;
+      case "/bookings":
+        tabName = "Bookings";
+        break;
+      case "/settings":
+        tabName = "Settings";
+        break;
+      default:
+        tabName = "WildPark";
+        break;
+    }
+    document.title = tabName;
+  }, [location.pathname]);
 
   return (
     <SessionUserContext.Provider value={sessionUserContextValue}>
