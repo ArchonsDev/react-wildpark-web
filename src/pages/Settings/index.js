f3eb82e0adb5d31e1c7612585fa742d21c737a7f
Dawn <dydawnraine@gmail.com>
2023-12-05 08:57:57 +0800

    adjusted the buttons

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import styles from "./style.module.css";
 
 const Settings = () => {
@@ -8,28 +8,33 @@ const Settings = () => {
       <div className={`${styles.backgroundContent} container-fluid`}>
         <div className={`${styles.content} container flex-column p-5`}>
           <div className={styles.cardsContainer}>
-            <Button>
-              Account Settings <br />
-              <span>Manage your details here</span>
-            </Button>
-            <br /> <br />
-            <Button>
-              Password & Security <br />
-              <span>Manage your privacy settings here</span>
-            </Button>
-            <br /> <br />
-            <Button>
-              Registered Vehicles <br />
-              <span>Manage your vehicles here</span>
-            </Button>
-            <br /> <br />
-            <Button>
-              Tickets & Citations <br />
-              <span>Manage your tickets and citations here</span>
-            </Button>
+            <div className="col">
+              <div className="row">
+                <Button>
+                  Account Settings <br />
+                  <p>Manage your details here</p>
+                </Button>
+              </div>
+              <div className="row">
+                <Button>
+                  Password & Security <br />
+                  <p>Manage your privacy settings here</p>
+                </Button>
+              </div>
+              <div className="row">
+                <Button>
+                  Registered Vehicles <br />
+                  <p>Manage your vehicles here</p>
+                </Button>
+              </div>
+              <div className="row">
+                <Button>
+                  Tickets & Citations <br />
+                  <p>Manage your tickets and citations here</p>
+                </Button>
+              </div>
+            </div>
           </div>
-
-          <div></div>
         </div>
       </div>
     </div>
