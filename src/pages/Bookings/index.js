77aa34ea483f46f2f26ad861f53b1f82e0890638
cxrvxxxx <brentempasisx3@gmail.com>
2023-12-11 18:49:18 +0800

    layout fix

--- src/pages/Bookings/index.js
+++ src/pages/Bookings/index.js
@@ -28,22 +28,30 @@ const Bookings = () => {
     <div className={styles.Bookings}>
       <div
         className={`${styles.content} container d-flex flex-column justify-content-end align-items-center`}>
+
         <div className={`${styles.dateTab} row d-flex justify-content-end`}>
           <div
             className={`${styles.date} col-sm-3 py-2 d-flex justify-content-center align-items-center`}>
             {currentDate.toLocaleString()}
           </div>
         </div>
-        <div className={`${styles.redBox} row pt-5 px-5 pb-0 mt-3`}>
-          <div
-            className={`${styles.whiteBox} container bg-white d-flex justify-content-center flex-column p-3`}>
-            <div className="row flex-grow-1">
-              <div className="col-sm-4 mb-2 d-flex align-items-center flex-column">
+
+        <div className={`${styles.redBox} row d-flex flex-column pt-5 px-5 pb-0 mt-3`}>
+          <div className={`${styles.whiteBox} flex-grow-1 col-md-12 container-fluid bg-white p-3`}>
+            <div className="row">
+
+              <div className="col-sm-3 mb-2 d-flex align-items-center flex-column">
                 <span className={styles.header}>Bookings</span>
-                <div className={`${styles.activeBookings} flex-grow-1`}></div>
+                <div className={`${styles.activeBookings} flex-grow-1 d-flex flex-column`}>
+                  <span className={styles.header}>Bookings</span>
+                  <span className={styles.header}>Bookings</span>
+                  <span className={styles.header}>Bookings</span>
+                  <span className={styles.header}>Bookings</span>
+                </div>
                 <BtnSecondary className="mt-auto">New Booking</BtnSecondary>
               </div>
-              <div className="col-sm-8 mb-2 d-flex align-items-center flex-column">
+
+              <div className="col-sm-9 mb-2 d-flex align-items-center flex-column">
                 <Card style={{ minWidth: "100%" }}>
                   <Card.Img variant="top" src={image} />
                   <ListGroup className={styles.bookingDetails}>
@@ -54,9 +62,11 @@ const Bookings = () => {
                   </ListGroup>
                 </Card>
               </div>
+
             </div>
           </div>
         </div>
+
       </div>
     </div>
   );
