ecf3216e5b2196dfa7f6c581b8b1cbb75b02f6c8
Dawn <dydawnraine@gmail.com>
2023-12-13 21:05:35 +0800

    added modal styling for logout and confirmation modal

--- src/common/LogoutModal/index.js
+++ src/common/LogoutModal/index.js
@@ -1,12 +1,13 @@
 import React, { useContext } from "react";
 import { Modal } from "react-bootstrap";
 
-import BtnPrimary from '../Buttons/BtnPrimary';
-import BtnSecondary from '../Buttons/BtnSecondary';
+import BtnPrimary from "../Buttons/BtnPrimary";
+import BtnSecondary from "../Buttons/BtnSecondary";
 
 import { useLogout } from "../../hooks/useLogout";
 
 import SessionUserContext from "../../contexts/SessionUserContext";
+import styles from "../ModalStyles/style.module.css";
 
 const LogoutModal = () => {
   const { showLogoutModal, toggleLogoutModal } = useContext(SessionUserContext);
@@ -18,14 +19,11 @@ const LogoutModal = () => {
       onHide={toggleLogoutModal}
       size="sm"
       aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Confirm
-        </Modal.Title>
+      centered>
+      <Modal.Header className={styles.title} closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">Confirm</Modal.Title>
       </Modal.Header>
-      <Modal.Body>
+      <Modal.Body className={styles.modalBody}>
         <p>Do you really want to logout?</p>
       </Modal.Body>
       <Modal.Footer>
@@ -36,6 +34,6 @@ const LogoutModal = () => {
       </Modal.Footer>
     </Modal>
   );
-}
+};
 
 export default LogoutModal;
