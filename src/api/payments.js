fc7faa40796727b6caec2f0ef3079806e4b50c97
Dawn <dydawnraine@gmail.com>
2023-12-17 01:19:45 +0800

    no im not

--- src/api/payments.js
+++ src/api/payments.js
@@ -4,23 +4,23 @@ import axios from "axios";
 export const addPayment = async (data, onSuccess, onFail, onCleanup) => {
   const currentDate = new Date();
   const year = currentDate.getFullYear();
-  const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-  const day = String(currentDate.getDate()).padStart(2, '0');
-  const hours = String(currentDate.getHours()).padStart(2, '0');
-  const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const day = String(currentDate.getDate()).padStart(2, "0");
+  const hours = String(currentDate.getHours()).padStart(2, "0");
+  const minutes = String(currentDate.getMinutes()).padStart(2, "0");
 
   try {
     const response = await axios.post(
       `http://localhost:8080/api/v1/payments/`,
       {
         ...data,
-        date: `${year}-${month}-${day} ${hours}:${minutes}`
+        date: `${year}-${month}-${day} ${hours}:${minutes}`,
       },
       {
         headers: {
-          "Authorization": `Bearer ${Cookies.get("userToken")}`,
-          "Content-Type": "application/json"
-        }
+          Authorization: `Bearer ${Cookies.get("userToken")}`,
+          "Content-Type": "application/json",
+        },
       }
     );
 
@@ -45,8 +45,6 @@ export const deletePayment = async (data, onSuccess, onFail, onCleanup) => {
         },
       }
     );
-
-    console.log("Delete response status:", response.status);
 
     if (response.status === 200) {
       onSuccess && onSuccess(response);
